40f017c3299776f0455066b23010d17e7d4bfb2a
edno <corentin.edno.blondiau@gmail.com>
2022-04-20 12:52:42 +0200

    Character answers data structure done
    Still need to compare them to player answer and figure out point system for final result

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ let attribute_left = [
     "NO",
     "YES",
     "YES"
-]
+];
 
 let attribute_right = [
     "NO",
@@ -66,14 +66,42 @@ let attribute_right = [
     "YES",
     "NO",
     "NO"
+];
+
+let characters = [];
+
+let characters_name = [
+    "Katlynn Maddox",
+    "Elise Woods",
+    "Charlie Tenelth",
+    "Vamir Erfolg",
+    "Ascal Vetag",
+    "Carenna Zakog",
+    "Iris Valentine",
+    "Beain Sinclair",
+    "Omra Danann",
+    "Monique Dancer"
 ]
 
+let character_answers = [
+    [45,25,5,80,5,60,10,0,20,10,95,55,55,15,100,15,10,10,65,55], // Katlynn
+    [75,55,70,75,70,15,80,95,0,80,45,85,20,10,20,55,95,80,5,100], // Elise
 
-let answers = []; // This will store answers to calculate matching character
+]
+
+class Character {
+    constructor(character_ID, character_name, answers) {
+        this.character_ID = character_ID;
+        this.character_name = character_name;
+        this.answers = answers;
+    }
+}
+
+let player_answers = []; // This will store player answers to calculate matching character
 
 let q_number = 0; // This will be the progression counter
 
-class q {
+class Q {
     constructor(q_number, prompt, attribute0, attribute1) {
         this.q_number = q_number;
         this.prompt = prompt;
@@ -82,12 +110,35 @@ class q {
     }
 
 }
-
-class answer {
-    constructor(q_number, answer_value) {
+class Answer {
+    constructor(q_number, answer_value, character_ID) {
         this.q_number = q_number;
         this.answer_value = answer_value;
+        this.character_ID = character_ID;
     }
+}
+
+function diff(a,b){return Math.abs(a-b);}
+
+function compute_result() {
+    for (let i=1; i < player_answers.length; i++) {
+        // pour chaque perso
+            //pour chaque reponse 1
+    }
+}
+
+let difference = []; // This list will contain the difference between the player's answer and every character's answer
+function compare_answer(player_answer) {
+
+    for (let i=0; i < player_answers.length; i++) {
+
+        let d = diff(player_answer.answer_value, characters[i].answer[q_number]);
+        console.log(d);
+        difference.push(d);
+
+    }
+
+    console.log(difference)
 }
 
 function show_slider() {
@@ -97,41 +148,40 @@ function show_slider() {
 
 setInterval(show_slider, 200);
 
-function start() {
-    document.getElementById("button").onclick = submit();
-}
 
-function diff(a,b){return Math.abs(a-b);}
-
-
-function compute_result() {
-    for (let i=1; i < answers.length; i++) {
-        // pour chaque perso
-            //pour chaque reponse 1
-    }
-}
-
-function compare_answer(q_number, answer_value) {
-    for (let i=0; i < answers.length; ) {
-        let difference = diff(answer_value, character[i].answer[q_number]);
-        console.log(difference);
-    }
-    diff()
-}
 
 function submit() {
+
+    if (q_number <= 0) { // Creates character objects at the start of the game
+
+        for (let i=0; i < characters_name.length; i++) {
+            let c = new Character(i, characters_name[i], character_answers[i]);
+            characters.push(c);
+        }
+        console.log(characters);
+    }
+
     if (q_number == 20) {
-        compute_result();
         document.getElementById("button").value = "FINISHED";
+        document.getElementById("score").style.opacity = 0;
+        document.getElementById("question").innerHTML = "Here is your result :";
+        document.getElementById("slider").style.opacity = 0;
+
+        compute_result();
         return;
     }
+
 
     let answer_value = document.getElementById("slider").value;
 
     document.getElementById("score").innerHTML = answer_value;
 
     console.log(answer_value);
-    answers.push(answer_value);
+    if (q_number > 0) {
+        player_answers.push(answer_value);
+    }
+    console.table(player_answers);
+
     document.getElementById("question").innerHTML = questions[q_number];
     document.getElementById("attribute_0").innerHTML = attribute_left[q_number];
     document.getElementById("attribute_1").innerHTML = attribute_right[q_number];
@@ -144,9 +194,8 @@ function submit() {
     document.getElementById("button").value = "SUBMIT";
     q_number = q_number+1;
     
-    console.table(answers);
-
-    compare_answer(q_number, answer_value);
+    let player_answer = new Answer(q_number, answer_value, "Player");
+    compare_answer(player_answer);
 
 
 }
